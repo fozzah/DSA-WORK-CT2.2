6d0424de9badb9940bca59bb11698d47afe4bba7
fozzah <144815131+fozzah@users.noreply.github.com>
2024-04-23 16:09:06 +0300

    Update and rename LinkedListCycle.java to LinkedListCycle.py

---
+++ CT Notes/Data Structures and Algorithms/DSA QUIZ 2/Question 1/LinkedListCycle.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def hasCycle(head):
+    if not head or not head.next:
+        return False
+    
+    slow = head
+    fast = head.next
+    
+    while fast and fast.next:
+        if slow == fast:
+            return True
+        slow = slow.next
+        fast = fast.next.next
+    
+    return False
+
